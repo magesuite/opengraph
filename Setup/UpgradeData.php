3a808227a9edda5dbac5f52b9033000a073c64fe
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2019-01-03 14:25:21 +0100

    Added UpgradeData to migrate attributes

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace MageSuite\Opengraph\Setup;
+
+class UpgradeData implements \Magento\Framework\Setup\UpgradeDataInterface
+{
+    /**
+     * @var \Magento\Eav\Setup\EavSetup
+     */
+    protected $eavSetup;
+
+    /**
+     * @var \Magento\Eav\Setup\EavSetupFactory
+     */
+    protected $eavSetupFactory;
+
+    /**
+     * @var \Magento\Framework\Setup\ModuleDataSetupInterface
+     */
+    protected $moduleDataSetupInterface;
+
+    public function __construct(
+        \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory,
+        \Magento\Framework\Setup\ModuleDataSetupInterface $moduleDataSetupInterface
+    )
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+        $this->moduleDataSetupInterface = $moduleDataSetupInterface;
+
+        $this->eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetupInterface]);
+    }
+
+    public function upgrade(
+        \Magento\Framework\Setup\ModuleDataSetupInterface $setup,
+        \Magento\Framework\Setup\ModuleContextInterface $context
+    )
+    {
+        if (version_compare($context->getVersion(), '1.0.3', '<')) {
+            $this->eavSetup->updateAttribute(
+                \Magento\Catalog\Model\Category::ENTITY,
+                'og_type',
+                'source_model',
+                \MageSuite\Opengraph\Model\Entity\Attribute\Source\Type::class
+            );
+
+            $this->eavSetup->updateAttribute(
+                \Magento\Catalog\Model\Product::ENTITY,
+                'og_type',
+                'source_model',
+                \MageSuite\Opengraph\Model\Entity\Attribute\Source\Type::class
+            );
+        }
+    }
+}
