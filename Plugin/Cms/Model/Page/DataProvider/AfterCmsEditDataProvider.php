d1e229102b39db327fa255ac58c3a37aa540d343
Grzegorz Bogusz <grzegorz.bogusz@creativestyle.pl>
2021-03-07 19:19:28 +0100

    Fixed og image in data provider on cms page in the admin panel

--- Plugin/Cms/Model/Page/DataProvider/AfterCmsEditDataProvider.php
+++ Plugin/Cms/Model/Page/DataProvider/AfterCmsEditDataProvider.php
@@ -4,12 +4,20 @@ namespace MageSuite\Opengraph\Plugin\Cms\Model\Page\DataProvider;
 class AfterCmsEditDataProvider
 {
     /**
+     * @var \Magento\Framework\App\RequestInterface
+     */
+    protected $request;
+
+    /**
      * @var \MageSuite\Opengraph\Service\Admin\CmsImageDataProvider
      */
     protected $cmsImageDataProvider;
 
-    public function __construct(\MageSuite\Opengraph\Service\Admin\CmsImageDataProvider $cmsImageDataProvider)
-    {
+    public function __construct(
+        \Magento\Framework\App\RequestInterface $request,
+        \MageSuite\Opengraph\Service\Admin\CmsImageDataProvider $cmsImageDataProvider
+    ) {
+        $this->request = $request;
         $this->cmsImageDataProvider = $cmsImageDataProvider;
     }
 
@@ -19,7 +27,13 @@ class AfterCmsEditDataProvider
             return $result;
         }
 
-        $pageData = reset($result);
+        $currentPageId = $this->request->getParam('page_id', 0);
+
+        if (!$currentPageId || !isset($result[$currentPageId])) {
+            return $result;
+        }
+
+        $pageData = $result[$currentPageId];
 
         if (!isset($pageData['og_image'])) {
             return $result;
