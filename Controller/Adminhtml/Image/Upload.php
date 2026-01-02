b37cf39818e74707102c66b1d6bd5fb33baa5d9c
Karolina Pawlus <karolina.pawlus@creativestyle.pl>
2026-01-02 07:24:31 +0000

    fix: [MGS-5640] Change backend controller parent class

--- Controller/Adminhtml/Image/Upload.php
+++ Controller/Adminhtml/Image/Upload.php
@@ -1,33 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MageSuite\Opengraph\Controller\Adminhtml\Image;
 
-class Upload extends \Magento\Framework\App\Action\Action
+class Upload extends \Magento\Backend\App\Action
 {
-    /**
-     * @var \MageSuite\Opengraph\Service\Processor\UploadImageFactory
-     */
-    protected $uploadImage;
-
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \MageSuite\Opengraph\Service\Processor\UploadImageFactory $uploadImage
+        protected \MageSuite\Opengraph\Service\Processor\UploadImageFactory $uploadImage
     ) {
         parent::__construct($context);
-        $this->uploadImage = $uploadImage;
     }
 
-    public function execute()
+    public function execute() //phpcs:ignore
     {
         try {
             $result = $this->uploadImage->create()->processUpload('og_image', \MageSuite\Opengraph\Service\CmsImageUrlProvider::OPENGRAPH_CMS_IMAGE_PATH);
         } catch (\Exception $e) {
             $result = ['error' => $e->getMessage(), 'errorcode' => $e->getCode()];
         }
+
         return $this->resultFactory->create(\Magento\Framework\Controller\ResultFactory::TYPE_JSON)->setData($result);
     }
 
-    protected function _isAllowed()
+    protected function _isAllowed() //phpcs:ignore
     {
         return true;
     }
